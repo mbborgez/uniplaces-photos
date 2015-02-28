21fdb2d0703eb537c15e4937682b3605316bd68c
Miguel Borges <mb.borgez@gmail.com>
2015-02-28 12:45:04 +0000

    login improvements

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -27,14 +27,20 @@ angular.module('starter.controllers', ['ionic', 'utils'])
             $scope.modal.show();
         };
 
+        $scope.logout = function() {
+            $localstorage.set('user', '');
+            $scope.loginData = {};
+            $scope.closeLogin();
+        }
+
         // Perform the login action when the user submits the login form
         $scope.doLogin = function () {
             $localstorage.set('user', $scope.loginData.username)
-            // Simulate a login delay. Remove this and replace with your login
-            // code if using a login system
-            $timeout(function () {
-                $scope.closeLogin();
-            }, 1000);
+            $scope.closeLogin();
+        };
+
+        $scope.isLogged = function() {
+            return loginData.username && loginData.password;
         };
 
     })
